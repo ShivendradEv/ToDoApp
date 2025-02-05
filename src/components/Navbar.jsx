d1851bd46745ad75468a9b2f52e8452c6d91cb6d
Shivendra <shivendrafc@gmail.com>
2025-02-05 23:04:24 +0530

    implemented login functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { NavLink } from "react-router";
+import AuthContext from '../context/AuthContext';
+import { useNavigate } from 'react-router';
+import { signOut } from "firebase/auth";
+import { auth } from '../firebase';
 
 const Navbar = () => {
+
+    const { user, setUser } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+
+    const handleLogout = async (e) => {
+        e.preventDefault();
+        try {
+            await signOut(auth);
+            navigate("/login");
+        } catch (error) {
+            console.error("Error signing out:", error);
+        }
+    };
+
     return (
         <div className='navbar'>
-            <p>ToDoApp</p>
+            <div className='navbar-brand'>
+                <a href='/'><img src='/assets/todo-logo.png' alt='logo' /> Taskly</a>
+            </div>
+            <ul className='nav-items'>
+                <li><NavLink to='/'>Home</NavLink></li>
+                {!user &&
+                    <>
+                        <li><NavLink to='/login'>Login</NavLink></li>
+                        <li><NavLink to='/signup'>Register</NavLink></li>
+                    </>
+                }
+                {user && <li className='logout'><a onClick={(e) => handleLogout(e)}>Logout</a></li>}
+            </ul>
         </div>
     )
 }
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='container'>
+            <div className='not-found'>
+                <h1>Oops! Page not found.</h1>
+                <p>The page you’re looking for doesn’t exist. Try going back to the <a href='/'>homepage.</a></p>
+            </div>
+        </div>
+    )
+}
+
+export default NotFound
